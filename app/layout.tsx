b96068f7190cde6a0cfa7748206b72de5773862b
Bryan Caraman <112526577+bryancaraman@users.noreply.github.com>
2024-08-03 12:36:00 -0400

    added analytics tracker

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "../global.css";
 import { Inter } from "@next/font/google";
 import LocalFont from "@next/font/local";
 import { Metadata } from "next";
-import { Analytics } from "./components/analytics";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: {
@@ -63,13 +63,14 @@ export default function RootLayout({
   return (
     <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
       <head>
-        <Analytics />
+        <title>Bryan Caraman</title>
       </head>
       <body
         className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
           }`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
